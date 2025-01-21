8920f6f804b5eeb82b09ae925fad3ad4ebe43a7e
Enzoputachi <samuelnzoputachi@gmail.com>
2025-01-21 15:40:24 +0100

    update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: {
+          100: '#F8F4F0',
+          500: '#F8F4F0',
+        },
+        Grey: {
+          100: '#F2F2F2',
+          300: '#B3B3B3',
+          500: '#696868',
+          900: '#201F24',
+        },
+      },
+    },
   },
   plugins: [],
 }
